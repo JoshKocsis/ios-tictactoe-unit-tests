01b9d1db71999f3af8fc23da805ada21d53e88ee
Josh Kocsis <josh.s.kocsis@gmail.com>
2020-07-28 20:27:45 -0500

    Guided project we went over yesterday.

--- TicTacToe/GameAI.swift
+++ TicTacToe/GameAI.swift
@@ -8,6 +8,67 @@
 
 import Foundation
 
-func game(board: GameBoard, isWonBy player: GameBoard.Mark) -> Bool {    
+func game(board: GameBoard, isWonBy player: GameBoard.Mark) -> Bool {
+
+    // Check verticals for wins
+
+    // Check each column for all three marks being the same player as the parameter to the function
+    for x in 0..<3 {
+        var markCount = 0
+        for y in 0..<3 {
+            if board[(x,y)] == player {
+                // They marked the board at this given location
+                markCount += 1
+            }
+        }
+
+        if markCount == 3 {
+            // They won
+            return true
+        }
+    }
+
+
+    // Check horizontals for wins
+    for y in 0..<3 {
+        var markCount = 0
+        for x in 0..<3 {
+            if board[(x,y)] == player {
+                // They marked the board at this given location
+                markCount += 1
+            }
+        }
+
+        if markCount == 3 {
+            // They won
+            return true
+        }
+    }
+
+    // Check diagonals for wins
+    let leftToRight = [(0,0), (1,1), (2,2)]
+    var ltrMarkCount = 0
+    for coordinate in leftToRight {
+        if board[coordinate] == player {
+            ltrMarkCount += 1
+        }
+    }
+
+    if ltrMarkCount == 3 {
+        return true
+    }
+
+    let rightToLeft = [(2,0), (1,1), (0,2)]
+    var rtlMarkCount = 0
+    for coordinate in rightToLeft {
+        if board[coordinate] == player {
+            rtlMarkCount += 1
+        }
+    }
+
+    if rtlMarkCount == 3 {
+        return true
+    }
+
     return false
 }
